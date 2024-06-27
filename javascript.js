78428a2977a7da9a23b562ba0ba01b8af05a3f67
Akshaygohrava <63100064+Akshaygohrava@users.noreply.github.com>
2024-06-27 23:27:26 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log("hello, chandraprakash");
+function down() {
+    let a = document.getElementById("demo");
+    a.style.height = "100vh";
+}
+function upfun() {
+    let b = document.getElementById("demo");
+    b.style.height = "0";
+}
